566a795fa73145e84d7ddcdd7119af9d3604c5f0
ccanokwuru <cccanoks@gmail.com>
2022-08-05 14:48:47 +0100

    fixed socket

--- src/routes/websocket/index.ts
+++ src/routes/websocket/index.ts
@@ -9,15 +9,6 @@ const websocketRoute: FastifyPluginAsync = async (
     const wsServer = this.websocketServer;
     const server = this.server;
 
-    server.on("broadcast", (payload) => {
-      const { meta, message } = payload;
-      if (meta === "notify") {
-        typeof message === "object"
-          ? sendToAll(JSON.stringify(message))
-          : sendToAll(message.toString());
-      }
-    });
-
     const sendToAll = (payload: any) =>
       // @ts-ignore
       wsServer.clients?.forEach((client) => {
@@ -30,12 +21,23 @@ const websocketRoute: FastifyPluginAsync = async (
         if (client !== wss && client.readyState === 1) client.send(payload);
       });
 
+    server.on("broadcast", (payload) => {
+      const { meta, message } = payload;
+      if (meta === "notify") {
+        typeof message === "object"
+          ? sendToAll(JSON.stringify(message))
+          : sendToAll(message.toString());
+      }
+    });
+
     wss.on("message", (m: string) => {
       const dataString = m.toString();
       const { meta, payload } = JSON.parse(dataString);
+      const source = request.headers["user-agent"];
 
-      if (meta === "join") sendToAll(payload);
-      else if (meta === "broadcast") broadcast(payload);
+      if (meta === "join") sendToAll(JSON.stringify({ source, payload }));
+      else if (meta === "broadcast")
+        broadcast(JSON.stringify({ source, payload }));
     });
 
     // @ts-ignore
